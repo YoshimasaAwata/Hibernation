fa5392d297229573aee4097557cbc5144319e530
YoshimasaAwata <yoshia.awata@nifty.ne.jp>
2024-10-08 00:13:45 +0900

    コーディング途中

---
+++ Hibernation/PowerConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hibernation
+{
+    class PowerConfig
+    {
+        public String Text { get; set; } = String.Empty;
+        public PowerConfig() { }
+
+        protected bool CallPowerCfg(String Arg)
+        {
+            bool rc = true;
+
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "powercfg";
+            info.Arguments = Arg;
+            info.RedirectStandardOutput = true;
+            info.UseShellExecute = false;
+            info.CreateNoWindow = true;
+
+            try
+            {
+                var cmd = Process.Start(info);
+                if (cmd != null)
+                {
+                    Text = cmd.StandardOutput.ReadToEnd();
+                    cmd.WaitForExit();
+                }
+            }
+            catch (Exception)
+            {
+                rc = false;
+            }
+
+            return rc;
+        }
+    }
+}
---
+++ Hibernation/SleepTimes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hibernation
+{
+    internal class SleepTimes
+    {
+        public UInt32 SleepTime { get; set; } = 0;
+        public UInt32 HibernationTime { get; set; } = 0;
+
+        public SleepTimes() { }
+
+
+    }
+}
