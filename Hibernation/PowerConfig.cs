37c9b455cd01bb436e48ae4865578190af06e6e9
YoshimasaAwata <yoshia.awata@nifty.ne.jp>
2024-10-29 12:11:06 +0900

    スリープ時間の設定を行うメソッドの追加 そのテストの一部を追加

--- Hibernation/PowerConfig.cs
+++ Hibernation/PowerConfig.cs
@@ -20,15 +20,21 @@ namespace Hibernation
         ///<value>GUIDを抽出するための正規表現</value>
         private static readonly string GuidRegEx = @"[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}";
         ///<value>電源管理の詳細設定を取得するオプション</value>
-        private static readonly string GetQuery = "/QUERY";
+        private static readonly string GetQuery = "/query";
+        ///<value>スリープの時間を設定するオプション</value>
+        private static readonly string Change = "/change";
         ///<value>アクティブな電源管理の設定を取得するオプション</value>
-        private static readonly string GetActiveScheme = "/GETACTIVESCHEME";
+        private static readonly string GetActiveScheme = "/getactivescheme";
         ///<value>スリープのGUIDのエイリアス</value>
         private static readonly string SubSleep = "SUB_SLEEP";
         ///<value>スタンバイのGUIDのエイリアス</value>
         private static readonly string StandbyIdle = "STANDBYIDLE";
         ///<value>休止状態のGUIDのエイリアス</value>
         private static readonly string HibernationIdle = "HIBERNATEIDLE";
+        ///<value>スタンバイの時間指定</value>
+        private static readonly string StandbyTimeoutAC = "standby-timeout-ac";
+        ///<value>休止状態の時間指定</value>
+        private static readonly string HibernationTimeoutAC = "hibernate-timeout-ac";
 
         ///<value>電源設定のGUID</value>
         protected string SchemeGUID { get; set; } = string.Empty;
@@ -36,7 +42,7 @@ namespace Hibernation
         protected string OutputText { get; set; } = string.Empty;
         /// <value>エラー発生時のメッセージ</value>
         public string ErrorMessage { get; set; } = string.Empty;
-        
+
         /// <summary>
         /// コンストラクタ
         /// </summary>
@@ -46,11 +52,9 @@ namespace Hibernation
         /// powercfgコマンドの実行
         /// </summary>
         /// <param name="options">powercfgコマンドのコマンドオプション</param>
-        /// <returns>
-        /// powercfgコマンドの実行結果</br>
-        /// true: 成功</br>
-        /// false: 失敗
-        /// </returns>
+        /// <returns>powercfgコマンドの実行結果</returns>
+        /// <value>true: 成功</value>
+        /// <value>false: 失敗</value>
         protected bool CallPowerCfg(string options)
         {
             bool rc = true;
@@ -104,9 +108,8 @@ namespace Hibernation
         /// 現在のスタンバイ/休止状態までの時間の設定を取得
         /// </summary>
         /// <param name="idle">スタンバイもしくは休止状態の指定</param>
-        /// <returns>
-        /// スタンバイ/休止状態までの時間(分)</br>
-        /// 取得できない場合には0(設定なし)が返る
+        /// <returns>スタンバイ/休止状態までの時間(分)</returns>
+        /// <value>0: 取得失敗(設定なし)</value>
         /// </returns>
         protected uint GetSleepTime(in String idle)
         {
@@ -119,7 +122,7 @@ namespace Hibernation
                 {
                     Match acPowerCfg = Regex.Match(OutputText, ACIndexRegEx);
                     Match hex = Regex.Match(acPowerCfg.Value, HexadecimalRegEx);
-                    time = Convert.ToUInt32(hex.Value, 16);
+                    time = Convert.ToUInt32(hex.Value, 16) / 60;
                 }
             }
             catch
@@ -130,27 +133,99 @@ namespace Hibernation
         }
 
         /// <summary>
+        /// 現在のスタンバイ/休止状態までの時間を設定
+        /// </summary>
+        /// <param name="timeout">スタンバイもしくは休止状態の指定</param>
+        /// <param name="time">スタンバイもしくは休止状態までの時間(分)</param>
+        /// <returns>スタンバイ/休止状態までの時間(分)</returns>
+        /// <value>負数: 設定失敗</value>
+        /// </returns>
+        protected int SetSleepTime(in String timeout, int time)
+        {
+            string options = Change + " " + timeout + " " + time.ToString();
+            bool rc = CallPowerCfg(options);
+            return rc ? time : -1;
+        }
+
+        /// <summary>
         /// 現在のスタンバイまでの時間の設定を取得
         /// </summary>
         /// <returns>
-        /// スタンバイまでの時間(分)</br>
-        /// 取得できない場合には0(設定なし)が返る
-        /// </returns>
+        /// <returns>スタンバイまでの時間(分)</returns>
+        /// <value>0: 取得失敗(設定なし)</value>
         public uint GetStandbyTime()
         {
             return GetSleepTime(StandbyIdle);
         }
 
         /// <summary>
+        /// スタンバイまでの時間を設定
+        /// </summary>
+        /// <param name="time">
+        /// スタンバイまでの時間(分)<br/>
+        /// 負数が指定された場合には0とみなす<br/>
+        /// デフォルト値: 0
+        /// </param>
+        /// <returns>設定した時間(秒)</returns>
+        /// <value>負数: 設定失敗</value>
+        public int SetStanbyTime(int time = 0)
+        {
+            int rc = -1;
+            if (time < 0)
+            {
+                time = 0;
+            }
+            var hibernation = GetHibernationTime();
+            if ((time == 0) || (hibernation > time))
+            {
+                rc = SetSleepTime(StandbyTimeoutAC, time);
+            }
+            else
+            {
+                ErrorMessage = "スタンバイ時間より休止状態時間が短い";
+            }
+            return rc;
+        }
+
+        /// <summary>
         /// 現在の休止状態までの時間の設定を取得
         /// </summary>
         /// <returns>
-        /// 休止状態までの時間(分)</br>
-        /// 取得できない場合には0(設定なし)が返る
+        /// <returns>スタンバイ/休止状態までの時間(分)</returns>
+        /// <value>0: 取得失敗(設定なし)</value>
         /// </returns>
         public uint GetHibernationTime()
         {
             return GetSleepTime(HibernationIdle);
+        }
+
+        /// <summary>
+        /// 休止状態までの時間を設定
+        /// </summary>
+        /// <param name="time">
+        /// 休止状態までの時間(分)<br/>
+        /// 負数が指定された場合には0とみなす<br/>
+        /// デフォルト値: 0
+        /// </param>
+        /// <returns>設定した時間(秒)</returns>
+        /// <value>負数: 設定失敗</value>
+        public int SetHibernationTime(int time = 0)
+        {
+            int rc = -1;
+            if (time < 0)
+            {
+                time = 0;
+            }
+            var stanby = GetStandbyTime();
+            if ((time == 0) || (stanby < time))
+            {
+                rc = SetSleepTime(HibernationTimeoutAC, time);
+            }
+            else
+            {
+                ErrorMessage = "スタンバイ時間より休止状態時間が短い";
+            }
+            return rc;
         }
     }
 }
--- HibernationTest/PowerConfigTest.cs
+++ HibernationTest/PowerConfigTest.cs
@@ -25,12 +25,12 @@ namespace HibernationTest
             Assert.True(String.IsNullOrEmpty(err));
         }
 
-        [Fact(DisplayName ="10:スタンバイ時間を確認"), Priority(10)]
+        [Fact(DisplayName = "10:スタンバイ時間を確認"), Priority(10)]
         public void SeeStandbyTime()
         {
             var pc = new PowerConfig();
-
             var err = pc.ErrorMessage;
+
             var time = pc.GetStandbyTime();
 
             Assert.True(String.IsNullOrEmpty(err));
@@ -41,12 +41,68 @@ namespace HibernationTest
         public void SeeHibernationTime()
         {
             var pc = new PowerConfig();
-
             var err = pc.ErrorMessage;
+
             var time = pc.GetHibernationTime();
 
             Assert.True(String.IsNullOrEmpty(err));
             Output.WriteLine("Standby time is {0}", time);
+        }
+
+        [Fact(DisplayName = "20:スタンバイ時間の設定確認"), Priority(20)]
+        public void SetStanbyTime()
+        {
+            var pc = new PowerConfig();
+            var err1 = pc.ErrorMessage;
+            if (String.IsNullOrEmpty(err1))
+            {
+                var current_time = (int)pc.GetStandbyTime();
+                var setting_time = pc.SetStanbyTime(0);
+                var err2 = pc.ErrorMessage;
+                if (String.IsNullOrEmpty(err2))
+                {
+                    var get_time = pc.GetStandbyTime();
+                    Assert.True(setting_time == 0);
+                    setting_time = pc.SetStanbyTime(current_time);
+                    Assert.True(setting_time == current_time);
+                }
+                else
+                {
+                    Assert.Fail(err2);
+                }
+            }
+            else
+            {
+                Assert.Fail(err1);
+            }
+        }
+
+        [Fact(DisplayName = "20:休止時間の設定確認"), Priority(20)]
+        public void SetHibernationTime()
+        {
+            var pc = new PowerConfig();
+            var err1 = pc.ErrorMessage;
+            if (String.IsNullOrEmpty(err1))
+            {
+                var current_time = (int)pc.GetHibernationTime();
+                var setting_time = pc.SetHibernationTime(0);
+                var err2 = pc.ErrorMessage;
+                if (String.IsNullOrEmpty(err2))
+                {
+                    var get_time = pc.GetHibernationTime();
+                    Assert.True(setting_time == 0);
+                    setting_time = pc.SetHibernationTime(current_time);
+                    Assert.True(setting_time == current_time);
+                }
+                else
+                {
+                    Assert.Fail(err2);
+                }
+            }
+            else
+            {
+                Assert.Fail(err1);
+            }
         }
     }
 }
