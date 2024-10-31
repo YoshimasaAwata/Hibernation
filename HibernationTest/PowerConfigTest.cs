8268aaa806099d3db88f82ab08579058d4812188
YoshimasaAwata <yoshia.awata@nifty.ne.jp>
2024-10-31 11:24:34 +0900

    Xunit.Extensions.Ordering導入 スタンバイ時間と休止時間がともに0の時にテスト失敗する不具合を修正

--- HibernationTest/PowerConfigTest.cs
+++ HibernationTest/PowerConfigTest.cs
@@ -1,13 +1,13 @@
 using Hibernation;
 using System.Printing;
 using Xunit.Abstractions;
-using Xunit.Priority;
+using Xunit.Extensions.Ordering;
 
 namespace HibernationTest
 {
-    [TestCaseOrderer(PriorityOrderer.Name, PriorityOrderer.Assembly)]
     public class PowerConfigTest
     {
+        static readonly int DefaultTime = 30;
         protected ITestOutputHelper Output { get; }
 
         public PowerConfigTest(ITestOutputHelper output)
@@ -15,7 +15,7 @@ namespace HibernationTest
             Output = output;
         }
 
-        [Fact(DisplayName = "0:初期化時にGUIDを正常に取得できるか"), Priority(0)]
+        [Fact(DisplayName = "0:初期化時にGUIDを正常に取得できるか"), Order(0)]
         public void CanGetGUIDSuccessfully()
         {
             var pc = new PowerConfig();
@@ -25,7 +25,7 @@ namespace HibernationTest
             Assert.True(String.IsNullOrEmpty(err), err);
         }
 
-        [Fact(DisplayName = "10:スタンバイ時間を確認"), Priority(10)]
+        [Fact(DisplayName = "10:スタンバイ時間を確認"), Order(10)]
         public void SeeStandbyTime()
         {
             var pc = new PowerConfig();
@@ -37,7 +37,7 @@ namespace HibernationTest
             Output.WriteLine("Standby time is {0}", time);
         }
 
-        [Fact(DisplayName = "10:休止時間を確認"), Priority(10)]
+        [Fact(DisplayName = "10:休止時間を確認"), Order(10)]
         public void SeeHibernationTime()
         {
             var pc = new PowerConfig();
@@ -49,7 +49,7 @@ namespace HibernationTest
             Output.WriteLine("Standby time is {0}", time);
         }
 
-        [Fact(DisplayName = "20:スタンバイ時間の設定確認"), Priority(20)]
+        [Fact(DisplayName = "20:スタンバイ時間の設定確認"), Order(20)]
         public void SetStanbyTime()
         {
             var pc = new PowerConfig();
@@ -67,7 +67,7 @@ namespace HibernationTest
             Assert.True(setting_time == current_time, "スタンバイ時間のセットに失敗");
         }
 
-        [Fact(DisplayName = "20:休止時間の設定確認"), Priority(20)]
+        [Fact(DisplayName = "20:休止時間の設定確認"), Order(20)]
         public void SetHibernationTime()
         {
             var pc = new PowerConfig();
@@ -95,7 +95,8 @@ namespace HibernationTest
             bool time_changed = false;
             if (target_time == 0)
             {
-                target_time = pc.SetHibernationTime(30);
+                int temp_time = current_time + DefaultTime;
+                target_time = pc.SetHibernationTime(temp_time);
                 time_changed = true;
             }
 
@@ -118,13 +119,13 @@ namespace HibernationTest
             }
         }
 
-        [Fact(DisplayName = "30:休止時間より長いスタンバイ時間をセット"), Priority(30)]
+        [Fact(DisplayName = "30:休止時間より長いスタンバイ時間をセット"), Order(30)]
         public void SetStandbyMoreThanHibernation()
         {
             SetStandbyMoreThanEqualHibernation(1);
         }
 
-        [Fact(DisplayName = "32:休止時間と同じスタンバイ時間をセット"), Priority(32)]
+        [Fact(DisplayName = "32:休止時間と同じスタンバイ時間をセット"), Order(32)]
         public void SetStandbyEqualHibernation()
         {
             SetStandbyMoreThanEqualHibernation(0);
@@ -137,11 +138,19 @@ namespace HibernationTest
             var current_time = (int)pc.GetHibernationTime();
             var target_time = (int)pc.GetStandbyTime();
 
-            bool time_changed = false;
+            bool standby_time_changed = false;
+            bool hibernation_time_changed = false;
             if (target_time == 0)
             {
-                target_time = pc.SetStanbyTime(30);
-                time_changed = true;
+                if (current_time <= time)
+                {
+                    int setting_test_time = DefaultTime + time + 1;
+                    int test_time = pc.SetHibernationTime(setting_test_time);
+                    Assert.True(test_time == setting_test_time, "休止時間の事前設定に失敗");
+                    hibernation_time_changed = true;
+                }
+                target_time = pc.SetStanbyTime(DefaultTime);
+                standby_time_changed = true;
             }
 
             var setting_time = pc.SetHibernationTime(target_time - time);
@@ -156,20 +165,24 @@ namespace HibernationTest
             }
             finally
             {
-                if (time_changed)
+                if (standby_time_changed)
                 {
                     pc.SetStanbyTime(0);
+                }
+                if (hibernation_time_changed)
+                {
+                    pc.SetHibernationTime(current_time);
                 }
             }
         }
 
-        [Fact(DisplayName = "35:スタンバイ時間より短い休止時間をセット"), Priority(35)]
+        [Fact(DisplayName = "35:スタンバイ時間より短い休止時間をセット"), Order(35)]
         public void SetHibernationLessThanStandby()
         {
             SetHibernationLessThanEqualStandby(1);
         }
 
-        [Fact(DisplayName = "36:スタンバイ時間と同じ休止時間をセット"), Priority(36)]
+        [Fact(DisplayName = "36:スタンバイ時間と同じ休止時間をセット"), Order(36)]
         public void SetHibernationEqualStandby()
         {
             SetHibernationLessThanEqualStandby(0);
